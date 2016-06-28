3ec36674b7013df9159669a2ecf83634ffbbb282
Chen Li <yichen.li0830@gmail.com>
2016-06-28 14:54:39 -0230

    27. Remove Element

---
+++ LeetCode27.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    var count = 0;
+    for (i=0;i<nums.length;i++){
+        if (nums[i]!==val) nums[count++] = nums[i];
+    }
+    return count;
+};
