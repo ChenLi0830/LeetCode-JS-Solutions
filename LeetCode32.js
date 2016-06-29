d21f62b990d0fb01e5e7f017f815a2dedd9afba4
Chen Li <yichen.li0830@gmail.com>
2016-06-29 18:31:56 -0230

    32. Longest Valid Parentheses - better approach

--- LeetCode32.js
+++ LeetCode32.js
@@ -6,7 +6,7 @@
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function(s) {
+var longestValidParentheses2 = function(s) {
     var leftCount = [], ans = 0, validLen = 0, valid=[], leftStack = [], stackIndex = 0;
     leftCount[-1] = 0;
     for (var i=0;i<s.length;i++){
@@ -38,10 +38,39 @@ var longestValidParentheses = function(s) {
     return ans;
 };
 
-console.assert(longestValidParentheses("")===0);
-console.assert(longestValidParentheses("(((((")===0);
-console.assert(longestValidParentheses(")))))")===0);
+var longestValidParentheses = function(s) {
+    var stack = [], stackCount = 0;
+    for (var i=0;i< s.length;i++){
+        if (s[i]==='('){
+            stackCount++;
+            stack[stackCount-1] = i;
+        } else { // s[i]===')'
+            if (s[stack[stackCount-1]]==='(') {// Find match pair
+                stackCount -- ;
+            } else {
+                stackCount++;
+                stack[stackCount-1] = i;
+            }
+        }
+    }
+
+    var ans = 0;
+    stack[-1] = -1; stackCount++; stack[stackCount-1] = s.length;
+    for (i=0;i<stackCount;i++){
+        ans = Math.max(ans, stack[i]-stack[i-1]-1);
+    }
+
+    return ans;
+};
+
+//console.assert(longestValidParentheses("")===0);
+//console.assert(longestValidParentheses("(((((")===0);
+//console.assert(longestValidParentheses(")))))")===0);
 console.assert(longestValidParentheses(")))))(((((")===0);
 console.assert(longestValidParentheses(")))))()(((((")===2);
 console.assert(longestValidParentheses(")()()(()))()")===8);
 console.assert(longestValidParentheses("()(()")===2);
+console.assert(longestValidParentheses("((((((()(()")===2);
+console.assert(longestValidParentheses("))()()(())")===8);
+console.assert(longestValidParentheses("()()())))")===6);
+console.assert(longestValidParentheses(")))(((())))(((")===8);
