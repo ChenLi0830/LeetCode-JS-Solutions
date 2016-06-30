56aa0b3d8e9e91f5e63b4c327fd92eda5bf32de1
Chen Li <yichen.li0830@gmail.com>
2016-06-30 10:53:04 -0230

    36. Valid Sudoku

--- LeetCode36.js
+++ LeetCode36.js
@@ -2,7 +2,7 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function(board) {
+var isValidSudoku2 = function(board) {
     for (var i=0;i<9;i++){
         var row = {}, col = {};
         for (var j=0;j<9;j++){
@@ -34,3 +34,32 @@ var isValidSudoku = function(board) {
 
     return true;
 };
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    var square = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+    for (var i=0;i<9;i++){
+        var row = {}, col = {};
+        for (var j=0;j<9;j++){
+            if (board[i][j]!=='.'){
+                if (row[board[i][j]]) return false;
+                row[board[i][j]] = true;
+
+                if (square[Math.floor(i/3)*3+Math.floor(j/3)][board[i][j]]) return false;
+                square[Math.floor(i/3)*3+Math.floor(j/3)][board[i][j]] = true;
+            }
+            if (board[j][i]!=='.'){
+                if (col[board[j][i]]) return false;
+                col[board[j][i]] = true;
+            }
+
+
+        }
+    }
+    return true;
+};
+
+isValidSudoku([".87654321","2........","3..5.....","4........","5........","6........","7........","8........","9........"]);
