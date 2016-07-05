6ec022306e07276a6df2f5d253e84ab8e4cf6f35
Chen Li <yichen.li0830@gmail.com>
2016-07-04 23:54:28 -0230

    62. Unique Paths less space complexity

--- LeetCode62.js
+++ LeetCode62.js
@@ -3,7 +3,7 @@
  * @param {number} n
  * @return {number}
  */
-var uniquePaths = function(m, n) {
+var uniquePaths2 = function(m, n) {
     let f = [];
     for (let i=0;i<m;i++){
         f[i] = []; f[i][0] = 1;
@@ -18,4 +18,18 @@ var uniquePaths = function(m, n) {
         }
     }
     return f[m-1][n-1];
+};
+
+var uniquePaths = function(m, n) {
+    let f = [];
+    for (let i=0;i<n;i++){
+        f[i] = 1;
+    }
+
+    for (i=1;i<m;i++){
+        for (j=1;j<n;j++){
+            f[j] = f[j] + f[j-1];
+        }
+    }
+    return f[n-1];
 };
