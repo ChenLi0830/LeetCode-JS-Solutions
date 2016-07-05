a65d55989e9031b49f75ba532a14fe300e470a67
Chen Li <yichen.li0830@gmail.com>
2016-07-04 23:06:30 -0230

    62. Unique Paths

---
+++ LeetCode62.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    let f = [];
+    for (let i=0;i<m;i++){
+        f[i] = []; f[i][0] = 1;
+    }
+    for (let i=0;i<n;i++){
+        f[0][i] = 1;
+    }
+
+    for (let i=1;i<m;i++){
+        for (let j=1;j<n;j++){
+            f[i][j] = f[i-1][j] + f[i][j-1];
+        }
+    }
+    return f[m-1][n-1];
+};
