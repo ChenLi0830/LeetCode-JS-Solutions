7d51c88d16776c360e35b255044f7c9b5d93ce3a
Chen Li <yichen.li0830@gmail.com>
2016-07-05 14:15:05 -0230

    65. Valid Number

---
+++ LeetCode65.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isNumber = function(s) {
+    s = s.trim();
+    let ans = false, matchStr;
+
+    matchStr = s.match(/(\+|\-)?(\d+(\.\d*)?|\d*\.\d+)(e(\+|\-)?\d+)?/);
+    ans = ans || matchStr && matchStr[0].length === s.length;
+    return !!ans;
+};
+
+
+console.assert(isNumber("1."));
+console.assert(isNumber(".1"));
+console.assert(isNumber("-.1"));
+console.assert(isNumber("-0"));
+console.assert(isNumber("12321"));
+console.assert(isNumber("-12321"));
+console.assert(isNumber("+12321"));
+console.assert(isNumber("-112312.12312"));
+console.assert(isNumber("+112312e-12312"));
+console.assert(isNumber("+112.312e-1212"));
+console.assert(isNumber("-.1e-12"));
+
+console.assert(!isNumber("."));
+console.assert(!isNumber("e9"));
+console.assert(!isNumber("abc"));
+console.assert(!isNumber("12a321"));
+console.assert(!isNumber("12 321"));
+console.assert(!isNumber("+112.312e-1212.123"));
