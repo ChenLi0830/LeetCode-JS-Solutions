03c37c44478c1ded91fe6c8611f04ec252d32aba
Chen Li <yichen.li0830@gmail.com>
2016-07-13 22:06:51 -0230

    106. Construct Binary Tree from Inorder and Postorder Traversal - use hash map to improve time efficiency

--- LeetCode106.js
+++ LeetCode106.js
@@ -29,7 +29,7 @@ var buildTree2 = function(inorder, postorder) {
     }
 };
 
-var buildTree = function(inorder, postorder) {
+var buildTree2 = function(inorder, postorder) {
     if (inorder.length===0) return null;
     return build(0, 0, inorder.length);
 
@@ -42,4 +42,20 @@ var buildTree = function(inorder, postorder) {
     }
 };
 
+
+var buildTree1 = function(inorder, postorder) {
+    if (inorder.length===0) return null;
+    let map = [];
+    for (let i=0;i<inorder.length;i++) {
+        map[inorder[i]] = i;
+    }
+    return build(0, 0, inorder.length);
+
+    function build(inS, poS, length){
+        let head = new TreeNode(postorder[poS+length-1]), leftL = map[head.val]-inS;
+        if (leftL>0) head.left = build(inS, poS, leftL);
+        if (length>leftL+1) head.right = build(inS+leftL+1, poS+leftL, length-1-leftL);
+        return head;
+    }
+};
 buildTree([2,4,1,5,3,7,9], [4,2,5,9,7,3,1]);
