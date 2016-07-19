ca6337fba7c3f506937003fcd88d517e6041f12a
Chen Li <yichen.li0830@gmail.com>
2016-07-18 22:27:06 -0230

    128. Longest Consecutive Sequence - a more concise and clear solution using Set

--- LeetCode128.js
+++ LeetCode128.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function(nums) {
+var longestConsecutive2 = function(nums) {
     let p = [], max = 0, f = [];
     for (let i=0;i<nums.length;i++) p[nums[i]] = true;
     for (let i=0;i<nums.length;i++){
@@ -19,6 +19,18 @@ var longestConsecutive = function(nums) {
                 f[j] = end - j;
             }
         }
+    }
+    return max;
+};
+
+
+var longestConsecutive = function(nums) {
+    let set = new Set(nums), max = 0;
+    for (let i=0;i<nums.length;i++){
+        if (set.has(nums[i]-1)) continue;
+        let start = nums[i], end = start;
+        while (set.has(end)) end++;
+        max = Math.max(end-start, max);
     }
     return max;
 };
