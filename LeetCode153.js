bc203303037f16e807ecef9812870579f8f9e7a5
Chen Li <yichen.li0830@gmail.com>
2016-07-25 16:12:59 -0230

    153. Find Minimum in Rotated Sorted Array

--- LeetCode153.js
+++ LeetCode153.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
+var findMin2 = function(nums) {
     return doSearch(0, nums.length-1);
     
     function doSearch(l,r){
@@ -14,3 +14,18 @@ var findMin = function(nums) {
         }
     }
 };
+
+var findMin = function(nums) {
+    let l = 0, r = nums.length-1;
+    while (l<r){
+        if (nums[l]<nums[r]) return nums[l];
+        let mid = Math.trunc((l+r)/2);
+        if (nums[mid]>nums[r]) {
+            l = mid+1;
+        } else {
+            r = mid;
+        }
+    }
+    return nums[l];
+};
+
