aab7a90d403b64a8778b4fa4f5fcb94c028442c8
Chen Li <yichen.li0830@gmail.com>
2016-07-25 00:33:26 -0230

    150. Evaluate Reverse Polish Notation - using stack

--- LeetCode150.js
+++ LeetCode150.js
@@ -2,7 +2,7 @@
  * @param {string[]} tokens
  * @return {number}
  */
-var evalRPN = function(tokens) {
+var evalRPN3 = function(tokens) {
     while (tokens.length>1){
         let i=0, newTokens = [];
         while(!isOperator(tokens[i])){
@@ -29,6 +29,68 @@ var evalRPN = function(tokens) {
     function isOperator(c){
         return c==="+" || c==="-" || c==="*" || c==="/";
     }
+};
+
+var evalRPN2 = function(tokens) {
+    let head = new DoubleListNode(-1), node = head, count = 0;
+    for (let i=0;i<tokens.length;i++){
+        if (!isOperator(tokens[i])) tokens[i] = parseInt(tokens[i]);
+        else count++;
+        node.next = new DoubleListNode(tokens[i]);
+        node.next.prev = node;
+        node = node.next;
+    }
+    
+    node = head.next; node.prev = null;
+    while (count>0){
+        while(!isOperator(node.val)){
+            node = node.next;
+        }
+        let num, n1 = node.prev.prev.val, n2 = node.prev.val;
+        switch (node.val){
+            case "+": num = n1+n2;break;
+            case "-": num = n1-n2;break;
+            case "*": num = n1*n2;break;
+            case "/": num = Math.trunc(n1/n2);break;
+        }
+        let curNode = node.prev.prev; curNode.val = num;
+        curNode.next = node.next;
+        if (node.next) node.next.prev = curNode;
+        
+        node = curNode;
+        count--;
+    }
+    return node.val;
+    
+    function isOperator(c){
+        return c==="+" || c==="-" || c==="*" || c==="/";
+    }
+};
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let s = [], index = 0;
+    for (let i=0;i<tokens.length;i++){
+        if (tokens[i]==="+"){
+            let n1 = s[--index], n2 = s[--index];
+            s[index++] = n2 + n1;
+        } else if (tokens[i]==="-"){
+            let n1 = s[--index], n2 = s[--index];
+            s[index++] = n2 - n1;
+        } else if (tokens[i] ==="*"){
+            let n1 = s[--index], n2 = s[--index];
+            s[index++] = n2 * n1;
+        } else if (tokens[i] ==="/"){
+            let n1 = s[--index], n2 = s[--index];
+            s[index++] = Math.trunc(n2/n1);
+        } else {
+            s[index++] = parseInt(tokens[i]);
+        }
+    }
+    return s[0];
 };
 
 evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]);
