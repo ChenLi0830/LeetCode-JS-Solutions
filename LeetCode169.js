8fe92a15a1461665779db82b4479697aa129b335
Chen Li <yichen.li0830@gmail.com>
2016-07-26 15:52:37 -0230

    169. Majority Element

---
+++ LeetCode169.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let map = new Map();
+    for (let i=0;i<nums.length;i++){
+        if (!map.has(nums[i])) map.set(nums[i],1);
+        else map.set(nums[i], map.get(nums[i])+1);
+        if (map.get(nums[i])>nums.length/2) return nums[i];
+    }
+};
