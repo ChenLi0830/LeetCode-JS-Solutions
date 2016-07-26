6df30375792ba514b4b35921c7f9f7acd2073a60
Chen Li <yichen.li0830@gmail.com>
2016-07-26 18:33:45 -0230

    169. Majority Element O(n) time and O(1) space complexity

--- LeetCode169.js
+++ LeetCode169.js
@@ -2,11 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+var majorityElement2 = function(nums) {
     let map = new Map();
     for (let i=0;i<nums.length;i++){
         if (!map.has(nums[i])) map.set(nums[i],1);
         else map.set(nums[i], map.get(nums[i])+1);
         if (map.get(nums[i])>nums.length/2) return nums[i];
     }
+};
+
+
+var majorityElement = function(nums) {
+    let majority, count = 0;
+    for (let i=0;i<nums.length;i++){
+        if (count===0){
+            majority = nums[i];
+            count++;
+        } else if (nums[i]!==majority){
+            count--;
+        } else {
+            count++;
+        }
+    }
+    return majority;
 };
