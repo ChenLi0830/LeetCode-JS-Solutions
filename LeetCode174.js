5741b6484248430dabd0c154d69b2c3e3fd4c032
Chen Li <yichen.li0830@gmail.com>
2016-07-27 15:21:08 -0230

    174. Dungeon Game

--- LeetCode174.js
+++ LeetCode174.js
@@ -3,51 +3,6 @@
  * @return {number}
  */
 var calculateMinimumHP2 = function(dungeon) {
-    let d = dungeon, fH = [], fA = [];
-    // Initialization
-    fH[-1] = []; fA[-1] = [];
-    for (let i=0;i<d.length;i++){
-        fA[i] = []; fH[i] = [];
-        fA[i][-1] = [-Infinity,-Infinity];
-        fH[i][-1] = [-Infinity,-Infinity];
-    }
-    for (let j=0;j<d[0].length;j++){
-        fA[-1][j] = [-Infinity,-Infinity];
-        fH[-1][j] = [-Infinity,-Infinity];
-    }
-    fA[0][-1] = [0,0];fH[0][-1] = [0,0];
-    
-    // DP Calculation
-    for (let i=0;i<d.length;i++){
-        for (let j=0;j<d[0].length;j++){
-            //fH
-            let value1 = Math.min(fH[i-1][j][0], fH[i-1][j][1]+d[i][j]);
-            let value2 = Math.min(fH[i][j-1][0], fH[i][j-1][1]+d[i][j]);
-            if (value1>value2) {
-                fH[i][j] = [value1, fH[i-1][j][1] +d[i][j]];
-            } else {
-                fH[i][j] = [value2, fH[i][j-1][1] +d[i][j]];
-            }
-            //fA
-            if (fA[i-1][j][1]>fA[i][j-1][1]){
-                fA[i][j] = [fA[i-1][j][0], fA[i-1][j][1]+d[i][j]];
-            } else {
-                fA[i][j] = [fA[i][j-1][0], fA[i][j-1][1]+d[i][j]];
-            }
-            if (fA[i][j][1]<fA[i][j][0]) fA[i][j][0] = fA[i][j][1];
-            //update fH from fA if fA is smaller
-            if (fA[i][j][0]>fH[i][j][0]) fH[i][j][0]=fA[i][j][0];
-        }
-    }
-    
-    return -fH[d.length-1][d[0].length-1][0]+1;
-};
-
-/**
- * @param {number[][]} dungeon
- * @return {number}
- */
-var calculateMinimumHP = function(dungeon) {
     let d = dungeon, f = [];
     // Initialization
     f[-1] = [];
@@ -98,6 +53,22 @@ var calculateMinimumHP = function(dungeon) {
     }
     f[d.length-1][d[0].length-1].sort((a,b)=>{return b[0]-a[0]});
     return -f[d.length-1][d[0].length-1][0][0]+1;
+};
+
+var calculateMinimumHP = function(dungeon) {
+    let m = dungeon.length, n = dungeon[0].length, f = [];
+    for (let i=m-1;i>=0;i--){
+        f[i] = [];
+        for (let j=n-1;j>=0;j--){
+            let need;
+            if (i===m-1 && j===n-1) need = 1 - dungeon[i][j];
+            else if (i===m-1) need = f[i][j+1] - dungeon[i][j];
+            else if (j===n-1) need = f[i+1][j] - dungeon[i][j];
+            else need = Math.min(f[i][j+1], f[i+1][j]) - dungeon[i][j];
+            f[i][j] = need <=0 ? 1: need;
+        }
+    }
+    return f[0][0];
 };
 
 // calculateMinimumHP([[-2,-3,-2,3],[-50,1,3,3],[50,50, 3, -100]]);
