d5065ab97934225eaa2ac2e0a96e28496bb3a210
Chen Li <yichen.li0830@gmail.com>
2016-07-28 14:39:00 -0230

    O(1) space, O(n) time

--- LeetCode198.js
+++ LeetCode198.js
@@ -2,10 +2,32 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
+var rob3 = function(nums) {
     let f = [];f[-1] = 0; f[-2] = 0; f[-3] = 0;
     for (let i=0;i<nums.length;i++){
         f[i] = Math.max(f[i-2]+nums[i], f[i-3]+nums[i]);
     }
     return Math.max(f[nums.length-2], f[nums.length-1]);
+};
+
+
+var rob2 = function(nums) {
+    let f = [];f[-1] = [0,0];
+    for (let i=0;i<nums.length;i++){
+        f[i] = [];
+        f[i][0] = Math.max(f[i-1][0], f[i-1][1]);
+        f[i][1] = f[i-1][0]+nums[i];
+    }
+    return Math.max(f[nums.length-1][0], f[nums.length-1][1]);
+};
+
+var rob = function(nums) {
+    let robLast, notRobLast, robCur=0, notRobCur=0;
+    for (let i=0;i<nums.length;i++){
+        robLast = robCur;
+        notRobLast = notRobCur;
+        robCur = notRobLast + nums[i];
+        notRobCur = Math.max(robLast, notRobLast);
+    }
+    return Math.max(robCur, notRobCur);
 };
