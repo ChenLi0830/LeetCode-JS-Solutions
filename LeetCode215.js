d20d453752645ddfb941370aea66a691ad927d03
Chen Li <yichen.li0830@gmail.com>
2016-08-01 19:28:02 -0230

    215. Kth Largest Element in an Array

--- LeetCode215.js
+++ LeetCode215.js
@@ -3,7 +3,7 @@
  * @param {number} k
  * @return {number}
  */
-var findKthLargest = function(nums, k) {
+var findKthLargest2 = function(nums, k) {
     ans = -1;
     return doSearch(nums, k);
     
@@ -22,4 +22,31 @@ var findKthLargest = function(nums, k) {
     }
 };
 
-findKthLargest()
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    ans = -1;
+    return doSearch(nums, k);
+    
+    function doSearch(nums, k){
+        let l = 0, r = nums.length-1, m = Math.trunc((l+r)/2),
+            pivot = nums.splice(m,1)[0],
+            left = [], right = [];
+        
+        for (let i=l;i<nums.length;i++){
+            if (nums[i]>pivot) left.push(nums[i]);
+            else right.push(nums[i]);
+        }
+        
+        if (left.length+1===k) return pivot;
+        else if (left.length+1>k) return doSearch(left, k);
+        else return doSearch(right, k-left.length-1);
+    }
+};
+
+
+
+findKthLargest([-1,-1], 2);
