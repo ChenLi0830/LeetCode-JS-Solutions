41280e6168c0771bef6780e35dfde09b2032b2e7
Chen Li <yichen.li0830@gmail.com>
2016-08-03 22:15:30 -0230

    219. Contains Duplicate II

---
+++ LeetCode219.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let map = new Map();
+    for (let i=0;i<nums.length;i++){
+        if (!map.has(nums[i])){
+            map.set(nums[i], i);
+        } else {
+            if (i-map.get(nums[i])<=k) return true;
+            map.set(nums[i], i);
+        }
+    }
+    return false;
+};
