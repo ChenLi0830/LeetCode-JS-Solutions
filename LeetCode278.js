07c758ed06f8481b0fa8bacb143dcf9401f11a35
Chen Li <yichen.li0830@gmail.com>
2016-08-16 22:46:54 -0230

    278. First Bad Version

---
+++ LeetCode278.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Chen on 2016-08-16.
+ */
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let l = 1, r = n;
+        while (l<r){
+            let m = Math.trunc((l+r)/2);
+            if (isBadVersion(m)){
+                r = m;
+            } else {
+                l = m+1;
+            }
+        }
+        return l;
+    };
+};
