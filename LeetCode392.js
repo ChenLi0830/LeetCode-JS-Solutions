582af68dd1b728780d6c0a07c85ffb817c10d492
Chen Li <yichen.li0830@gmail.com>
2016-09-27 15:46:07 +0800

    392. Is Subsequence

---
+++ LeetCode392.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+  let j = 0;
+  for (let i=0;i<t.length;i++){
+    if (j<s.length && t[i]===s[j]) j++;
+  }
+  return j===s.length;
+};
+
