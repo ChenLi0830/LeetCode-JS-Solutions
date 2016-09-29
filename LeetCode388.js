26b747091b9417f5ba0a3687e998922546b9de8f
Chen Li <yichen.li0830@gmail.com>
2016-09-29 11:04:30 +0800

    388. Longest Absolute File Path - shorter solution using split and lastIndexOf methods

--- LeetCode388.js
+++ LeetCode388.js
@@ -2,7 +2,7 @@
  * @param {string} input
  * @return {number}
  */
-var lengthLongestPath = function(input) {
+var lengthLongestPath2 = function(input) {
   let ans = 0, arr = [];
   if (input.indexOf("\n") === -1) {
     return (input.indexOf(".") === -1) ? 0 : input.length;
@@ -45,7 +45,23 @@ var lengthLongestPath = function(input) {
   }
 };
 
-
+var lengthLongestPath = function(input) {
+  let ans = 0, arr = [],
+      strs = input.split("\n");
+  
+  strs.forEach(s => {
+    let i = s.lastIndexOf("\t") + 1;
+    arr[i] = s.length - i; // length of the path
+    
+    if (s.indexOf(".")>-1) {
+      let lengthSum = 0;
+      for (let j=0; j <= i;j++) lengthSum += arr[j];
+      ans = Math.max(ans, lengthSum + i);
+    }
+  });
+  return ans;
+};
 
 // lengthLongestPath("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext");
-lengthLongestPath("aaaaaa");
+// lengthLongestPath("aaaaaa");
+lengthLongestPath("a\n\taa\n\t\taaa\n\t\t\tfile1.txt\naaaaaaaaaaaaaaaaaaaaa\n\tsth.png");
