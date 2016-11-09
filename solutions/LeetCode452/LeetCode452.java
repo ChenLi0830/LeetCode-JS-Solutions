244378ca9b8491ec25a868edf8c1d7a7fc923bfd
Chen Li <yichen.li0830@gmail.com>
2016-11-08 21:43:55 -0800

    453. Minimum Moves to Equal Array Elements - Java

---
+++ solutions/LeetCode452/LeetCode452.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i<nums.length; i++){
+            if (min>nums[i]) min = nums[i];
+        }
+        int ans = 0;
+        for (int i=0; i<nums.length; i++){
+            ans += nums[i]-min;
+        }
+        return ans;
+    }
+}
