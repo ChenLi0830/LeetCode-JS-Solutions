06eaa26dcce6833d0a51f17444b2c807a8025140
Chen Li <yichen.li0830@gmail.com>
2016-11-17 21:30:53 -0800

    LeetCode 2nd time

--- solutions/leetCode1.js
+++ solutions/leetCode1.js
@@ -6,7 +6,7 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+var twoSum2 = function (nums, target) {
   var dict=[];
   for (var i = 0;i<nums.length;i++){
     const otherNum = target-nums[i];
@@ -19,3 +19,20 @@ var twoSum = function (nums, target) {
 
 console.log(twoSum([2, 7, 11, 15],9));
 console.log(twoSum([3,2,4],6));
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = (nums, target) => {
+  let hashMap = new Map();
+  for (let i=0; i<nums.length; i++){
+    if (hashMap.has(target - nums[i])){
+      return [hashMap.get(target - nums[i]), i]
+    } else {
+      hashMap.set(nums[i], i);
+    }
+  }
+  return [];
+};
